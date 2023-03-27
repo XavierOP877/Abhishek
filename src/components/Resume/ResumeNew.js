7d00a01e2e2282a78d1263b1de81d9d638ed0261
Abhishek Tripathi <95579106+XavierOP877@users.noreply.github.com>
2023-03-27 23:38:23 +0530

    app update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/XavierOP877/Website/main/src/Assets/AbhishekTripathi_Resume%20(3).pdf";
+  "https://raw.githubusercontent.com/XavierOP877/Abhishek/main/src/Assets/AbhishekTripathi_Resume%20(3).pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
